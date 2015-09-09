ef4a77cc6cc5b6296469eb8a88df77189bf0064f
zhouxiaomin <946574585@qq.com>
2015-09-09 09:57:32 +0800

    create

---
+++ Desktop/gitTestTwo/gitTestTwo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  gitTestTwo
+//
+//  Created by 周晓敏 on 15/9/9.
+//  Copyright (c) 2015年 周晓敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Desktop/gitTestTwo/gitTestTwo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gitTestTwo
+//
+//  Created by 周晓敏 on 15/9/9.
+//  Copyright (c) 2015年 周晓敏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
